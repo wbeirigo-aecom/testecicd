d8e1dd014195e25dfb33d207b1b16d7213d07e7b
Duarte, Wilson Beirigo <wilson.duarte@aecom.com>
2025-10-18 14:14:31 -0300

    ajustes

--- src/main.py
+++ src/main.py
@@ -18,3 +18,5 @@ def read_item(item_id: int, q: Union[str, None] = None):
 async def get_config():
     api_key = os.getenv("MY_MESSAGE", "default_api_key") # "default_api_key" is a fallback if API_KEY is not set
     return {"my_message": api_key}
+
+
