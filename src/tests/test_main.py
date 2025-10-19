857d143d2aef72cedbc2f9ee4a67e4960d04b3cd
Duarte, Wilson Beirigo <wilson.duarte@aecom.com>
2025-10-19 15:43:28 -0300

    aaaajustess

---
+++ src/tests/test_main.py
@@ -0,0 +1,14 @@
+def test_dois_basic():
+    assert 2 + 2 == 4
+
+
+def test_dois_addition():
+    assert 8 + 2 == 10
+
+
+def test_dois_large():
+    assert 2 + 22 == 24
+
+
+def test_dois_extended():
+    assert 2 + 82 == 84
